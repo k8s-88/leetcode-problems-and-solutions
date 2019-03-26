50ad230b2fb5118cb71a6c57df0ccc54fcae5a29
Katie O Donnell <katiemodonnell88@gmail.com>
2019-03-26 12:39:12 +0000

    problem 1 solution

--- strings/TwoSum.js
+++ strings/TwoSum.js
@@ -9,4 +9,15 @@
 // Because nums[0] + nums[1] = 2 + 7 = 9,
 // return [0, 1].
 
-
+function twoSum(nums, target) {
+    res = []
+    for (var i = 0; i < nums.length; i++) {
+        for (var j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                res.push(i, j)
+            }
+        }
+    }
+    return res
+}
+console.log(twoSum([3, 2, 4], 6))
