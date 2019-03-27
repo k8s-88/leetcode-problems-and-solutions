42a55f7fdfac0c4305921c9e6281bc9af8dbe494
Katie O Donnell <katiemodonnell88@gmail.com>
2019-03-27 09:33:20 +0000

    lengthOfLastWord solution added

---
+++ strings/LengthOfLastWord.js
@@ -0,0 +1,17 @@
+// Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+
+// If the last word does not exist, return 0.
+
+// Note: A word is defined as a character sequence consists of non-space characters only.
+
+// Example:
+
+// Input: "Hello World"
+// Output: 5
+
+
+
+var lengthOfLastWord = function(s) {
+    const arr = s.trim().split(' ');
+    return arr.pop().length;
+};
